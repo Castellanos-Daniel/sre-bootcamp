3d3e42f2b07496464fba91acd82961ec6c5c622f
Gerardo-Castellanos <gerardo.castellanos@ibm.com>
2021-08-25 18:39:41 -0500

    modified tests

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -1,5 +1,7 @@
+from os import getenv
 import unittest
 from methods import Token, Restricted
+from dotenv import load_dotenv, find_dotenv
 
 
 class TestStringMethods(unittest.TestCase):
@@ -7,12 +9,25 @@ class TestStringMethods(unittest.TestCase):
     def setUp(self):
         self.convert = Token()
         self.validate = Restricted()
+        load_dotenv(find_dotenv())
+        self.test_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4ifQ.BmcZ8aB5j8wLSK8CqdDwkGxZfFwM1X1gfAIN7cXOx9w'
 
     def test_generate_token(self):
-        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
+        self.assertEqual(self.test_token,
+                         self.convert.generate_token('admin', 'secret'))
+
+    def test_generate_token_failsafe(self):
+        self.assertEqual(False,
+                         self.convert.generate_token('fake_user', 'incorrect_pass'))
 
     def test_access_data(self):
-        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+        self.assertEqual('You are under protected data',
+                         self.validate.access_data(self.test_token))
+
+    def test_access_data_failsafe(self):
+        self.assertEqual(False,
+                         self.validate.access_data('falseToken'))
+
 
 if __name__ == '__main__':
     unittest.main()
